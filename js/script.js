1165810d9eb95e73caf55109664d366e9b4c44a4
ivan1986mrpl <121858281+ivan1986mrpl@users.noreply.github.com>
2023-10-04 10:26:45 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+/* ============= MENU =============== */
+/* const menu = document.querySelector('.menu__body')
+const menuBtn = document.querySelector('.menu__icon')
+
+const body = document.body;
+
+if (menu && menuBtn) {
+	menuBtn.addEventListener('click', e => {
+		menu.classList.toggle('active')
+		menuBtn.classList.toggle('active')
+		body.classList.toggle('lock')
+	})
+	
+	menu.addEventListener('click', e => {
+		if (e.target.classList.contains('menu__body')) {
+			menu.classList.remove('active')
+			menuBtn.classList.remove('active')
+			body.classList.remove('lock')
+		}
+	}) */
+/* ============= /MENU =============== */
+
+/* ============= Переход к секции =============== */
+	/* menu.querySelectorAll('.menu__link').forEach(link => {
+		link.addEventListener('click', () => {
+			menu.classList.remove('active')
+			menuBtn.classList.remove('active')
+			body.classList.remove('lock')
+		})
+	})
+} */
+/* ============= Переход к секции =============== */
+
+/* ============= Плавный скролл к id ============ */
+/* const anchors = document.querySelectorAll('a[href*="#"]');
+
+anchors.forEach(anchor => {
+	anchor.addEventListener('click', event => {
+		event.preventDefault();
+
+		const blockID = anchor.getAttribute('href').substring(1);
+
+		document.getElementById(blockID).scrollIntoView({
+			behavior: 'smooth',
+			block: 'start'
+		})
+	})
+}) */
+/* ============= Плавный скролл к id ============ */
+'use strict';
+
+const reviewsSwiper = document.querySelector('.swiper-reviews');
+
+if (reviewsSwiper) {
+	const swiper = new Swiper('.swiper-reviews', { 
+		// Optional parameters
+		loop: true,
+		autoHeight: true,
+		// If we need pagination
+		pagination: {
+		  el: '.swiper-pagination',
+		  clickable: true,
+		},
+	});
+}
+
+
